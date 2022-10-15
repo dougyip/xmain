24e97dac4dc4d2f900888875b42d02feb00272f2
dougyip <doug.yip@colbyinstruments.com>
2022-10-15 00:26:42 -0700

    Add relay initialization and delay algorithms

--- channelbuilder.py
+++ channelbuilder.py
@@ -40,23 +40,43 @@ class Trombone(DelayComponent):
 class Relay(DelayComponent):
     sections: list
     top_up: list
-    _range = int
+    _binary_sections_range = int
     _step = int
     __str__ = "RELAY"
+    _i2c_address_map = {}
 
     def initialize(self):            
-        self._range = 2 * self.sections[-1][0]
+        self._binary_sections_range = 2 * self.sections[-1][0]
         self._step = self.sections[0][0]
         print(f"   Initializiing Relay:")       
         print(f"   Relay has step of {self._step}")
-        print(f"   Relay has range of {self._range}")
+        print(f"   Relay has range of {self._binary_sections_range}")
         print(f"   Relay has top_up {self.top_up}")
 
+        for section in self.sections: self._i2c_address_map[section[1]] = 0b00
+        if self.top_up is not None: self._i2c_address_map[self.top_up[1]] = 0b00
+        print(f"   {self._i2c_address_map}")
+
     def set_delay(self, val):
-        if val > self._range:
+        # Check if the top_up Relay section needs to be activated     
+        if val > self._binary_sections_range:
+            self._i2c_address_map[self.top_up[1]] ^= self.top_up[2]
             print(f"   Relay has top_up of {self.top_up[0]:,} turned on")
             val = val - self.top_up[0]
+        elif self.top_up is not None:
+            self._i2c_address_map[self.top_up[1]] &= ~(self.top_up[2])
+        # Now determine which binary sections need to be activated
+        val /= 1000
+        bit_pointer = 0b1
+        for list_item in self.sections:
+            print(f"   val = {int(val)} bit pointer = {bit_pointer}")
+            if int(val) & bit_pointer:
+                self._i2c_address_map[list_item[1]] ^= list_item[2]
+            else:
+                self._i2c_address_map[list_item[1]] &= ~(list_item[2])        
+            bit_pointer = bit_pointer << 1
 
+        print(f"   New {self._i2c_address_map}")
         
         print(f"   Relay binary sections are now set to {val:,}")
 
@@ -96,7 +116,7 @@ class Channel(DelayComponent):
             t_val = 0
             # Determine delay value for Trombone if this channel has one
             if self._trombone_index is not None:
-                if self.components[self._relay_index] is not None and val > self.components[self._relay_index]._range:
+                if self.components[self._relay_index] is not None and val > self.components[self._relay_index]._binary_sections_range:
                     t_val = (val - self.components[self._relay_index].top_up[0]) % self.components[self._trombone_index].size           
                 else:
                     t_val = val % self.components[self._trombone_index].size
@@ -135,7 +155,7 @@ async def main() -> None:
 
     for channel in channels: channel.initialize()
     channels[constants.CH1].set_delay(627000)
-    channels[constants.CH2].set_delay(45000000)
+    channels[constants.CH2].set_delay(165000000)
 
 if __name__ == "__main__":
     asyncio.run(main())
