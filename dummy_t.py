1934640fd34ef322e81a7c0a868a7922f3c9255a
Victor Chinn <victor.chinn@colbyinstruments.com>
2022-10-19 05:48:06 +0100

    set_delay returns int Error Code

--- dummy_t.py
+++ dummy_t.py
@@ -2,7 +2,7 @@
 import asyncio
 import time
 
-def set_trombone_delay(address, val, overshoot, use_cal_table, callback):
+def set_trombone_delay(address, val, overshoot, use_cal_table, callback) -> int:    # RETURNS ERR_ CODE
     print(f"Setting trombone {address} to {val:,}")
     time.sleep(.1)
     callback(1)
